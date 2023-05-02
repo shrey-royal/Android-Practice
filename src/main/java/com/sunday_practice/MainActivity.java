ac1a9462f1092d51ead481ed4dbeb62bee571f49
Shrey K <srkkadia@gmail.com>
2023-05-02 18:54:27 +0530

    Custom Dropdown, Custom Buttons

--- src/main/java/com/sunday_practice/MainActivity.java
+++ src/main/java/com/sunday_practice/MainActivity.java
@@ -3,6 +3,11 @@ package com.sunday_practice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String data[] = {"Java", "Python", "C++", "C", "Android", "JavaScript"};
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.spinner_selected_text, data);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown);
+
+        Spinner spinner = findViewById(R.id.spinner);
+
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                Toast.makeText(MainActivity.this, adapterView.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
     }
 }
