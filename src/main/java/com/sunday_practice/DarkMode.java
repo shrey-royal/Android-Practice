55c33687fac3f06972f2802aff07ff2e6c947918
Shrey K <srkkadia@gmail.com>
2023-05-23 18:48:13 +0530

    dark-theme-shared-preference

--- src/main/java/com/sunday_practice/DarkMode.java
+++ src/main/java/com/sunday_practice/DarkMode.java
@@ -2,13 +2,50 @@ package com.sunday_practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class DarkMode extends AppCompatActivity {
 
+    private Switch mySwitch;
+    SharedPref sharedPref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        sharedPref = new SharedPref(this);
+        if(sharedPref.loadNightModeState() == true) {
+            setTheme(R.style.Theme_light);
+        } else setTheme(R.style.Theme_light);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dark_mode);
+
+        mySwitch = (Switch) findViewById(R.id.mySwitch);
+        if(sharedPref.loadNightModeState() == true) {
+            mySwitch.setChecked(true);
+        }
+        mySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked) {
+                    sharedPref.setNightModeState(true); //changing the App theme to darkMode
+                    System.out.println("DarkMode Set");
+                    restartApp();
+                } else {
+                    sharedPref.setNightModeState(false);
+                    System.out.println("LightMode Set");
+
+                    restartApp();
+                }
+            }
+        });
+    }
+
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(), DarkMode.class);
+        startActivity(i);
+        finish();   //destroy the current activity
     }
 }
