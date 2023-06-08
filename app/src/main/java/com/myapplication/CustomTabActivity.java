4c12c120387ccef304fa488ed509fc21191f6dce
Shrey K <srkkadia@gmail.com>
2023-06-08 09:25:35 +0530

    DarkMode

---
+++ app/src/main/java/com/myapplication/CustomTabActivity.java
@@ -0,0 +1,14 @@
+package com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_tab);
+    }
+}
