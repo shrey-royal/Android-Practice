1b256a1e22ee16c1c8851416bf1d8a3a0ed48143
Shrey K <srkkadia@gmail.com>
2023-06-27 19:03:22 +0530

    Bottom Nav Bar

---
+++ app/src/main/java/com/myapplication/MainBottomNavBar.java
@@ -0,0 +1,14 @@
+package com.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainBottomNavBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bottom_nav_bar);
+    }
+}
